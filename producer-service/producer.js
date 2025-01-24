8a08a72085f1a0928a7cb5237fc7a85cbe499daa
Ashik MP <55047798+ashvegeta@users.noreply.github.com>
2025-01-24 12:24:27 -0800

    added basic kafka prod/consumer

--- producer-service/producer.js
+++ producer-service/producer.js
@@ -1,3 +1,35 @@
-async function produce(params) {}
+const { Kafka } = require("kafkajs");
+
+async function produce() {
+  try {
+    // Define Kafka client configuration
+    const kafka = new Kafka({
+      clientId: "polygrid-producer",
+      brokers: ["localhost:9092"],
+    });
+
+    // Create a producer instance
+    const producer = kafka.producer();
+
+    // Connect to the Kafka broker
+    await producer.connect();
+
+    // Send messages to a topic
+    let counter = 1;
+
+    setInterval(async () => {
+      await producer.send({
+        topic: "analytics",
+        messages: [{ value: "Hello Kafka from Node.js! " + counter }],
+      });
+      counter += 1;
+    }, 1000);
+
+    // Disconnect from the Kafka broker
+    // await producer.disconnect();
+  } catch (error) {
+    console.error(`Error producing message: ${error.message}`);
+  }
+}
 
 produce();
